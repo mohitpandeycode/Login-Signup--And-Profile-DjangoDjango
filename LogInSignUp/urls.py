28adbb7a6417c2ade79a6e4c4f7f85f6fc755ba8
Mohit Pandey <mp7698921@gmail.com>
2024-04-09 19:17:33 +0530

    h

--- LogInSignUp/urls.py
+++ LogInSignUp/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf.urls import url  # Add this import for 'url'
 
 urlpatterns = [
+    url(r'^jet/', include(('jet.urls', 'jet'), namespace='jet')),  # Django JET URLS
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
 ]
