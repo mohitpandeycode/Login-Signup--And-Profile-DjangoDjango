c3070e40211695da694b1275628fd51d6e3f4b0a
Mohit Pandey <mp7698921@gmail.com>
2024-04-09 14:07:37 +0530

    hostw

--- LogInSignUp/settings.py
+++ LogInSignUp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8&*xl=w1%6em+=zgn6fdf1111(a*(wjndp#58s@oi%kq@ath9b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
